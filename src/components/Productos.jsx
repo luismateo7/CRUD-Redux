1c13017a65cd8752b4301ea3b9aa89da7e0bc201
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-28 03:02:56 -0500

    Dispatch para obtener los productos de la DB

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+
+//Redux
+import { useDispatch, useSelector } from "react-redux"
+import { obtenerProductosAction } from "../actions/productoActions"
 
 export default function Productos() {
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    //Consultar la API
+    const cargarProductos = () => dispatch ( obtenerProductosAction() );
+    cargarProductos();
+  }, [])
+
   return (
     <>
       <h2 className="text-center my-5">Listado de Productos</h2>
