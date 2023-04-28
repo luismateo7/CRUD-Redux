8b60b09ef4f24e91b0fd664d9bd9879e9ae8e8f5
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-28 02:09:35 -0500

    Provider obteniendo los datos del Store

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,27 @@ import Productos from "./components/Productos";
 import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 
+//REDUX
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/">
-          <Route index element={<Productos />} />
-        </Route>
+      <Provider store={store}>
+        <Header />
+        <Routes>
+          <Route path="/">
+            <Route index element={<Productos />} />
+          </Route>
 
-        <Route path="/productos">
-          <Route path="nuevo" element={<NuevoProducto />} />
-          <Route path="editar/:id" element={<EditarProducto />} />
-        </Route>
+          <Route path="/productos">
+            <Route path="nuevo" element={<NuevoProducto />} />
+            <Route path="editar/:id" element={<EditarProducto />} />
+          </Route>
 
-      </Routes>
+        </Routes>
+      </Provider>
     </BrowserRouter>
   );
 }
