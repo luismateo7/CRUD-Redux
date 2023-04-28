ead72d1f31f4077ab3c846ca3b70aa169e0fcb9d
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-27 23:31:02 -0500

    Routing a NuevoProducto y Editar Producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+//Componentes
 import Header from "./components/Header";
 import Productos from "./components/Productos";
+import NuevoProducto from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Header />}>
+        <Route path="/">
           <Route index element={<Productos />} />
         </Route>
+
+        <Route path="/productos">
+          <Route path="nuevo" element={<NuevoProducto />} />
+          <Route path="editar/:id" element={<EditarProducto />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
