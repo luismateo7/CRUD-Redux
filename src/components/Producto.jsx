c343af1104ab32376085416a9128f850a0832b8e
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-29 13:25:47 -0500

    useDispatch llama al action de eliminar producto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,8 +1,34 @@
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { eliminarProductosAction } from "../actions/productoActions";
+import Swal from "sweetalert2";
 
 export default function Producto({ producto }) {
 
+    //Utilizar useDispatch y te crea una función
+    const dispatch = useDispatch();
+
     const { nombre, precio, id } = producto;
+
+    const confirmarEliminarProducto = id => {
+
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto que se elimina no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //Pasarlo al action
+                dispatch(eliminarProductosAction(id))
+
+            }
+        })
+    }
 
     return (
         <tr className="text-center">
@@ -13,6 +39,7 @@ export default function Producto({ producto }) {
                 <button
                     type="button"
                     className="btn btn-danger"
+                    onClick={() => confirmarEliminarProducto(id)}
                 >Eliminar</button>
             </td>
         </tr>
