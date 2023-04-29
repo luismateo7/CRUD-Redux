8a5b60a49f03c2e1cc4e0b690adaab5405fe7fa1
Mateo Rodriguez <luismateor73@gmail.com>
2023-04-28 23:05:25 -0500

    Obtener el state de los productos y mostrarlos

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -3,24 +3,33 @@ import { useEffect } from "react";
 //Redux
 import { useDispatch, useSelector } from "react-redux"
 import { obtenerProductosAction } from "../actions/productoActions"
+import Producto from "./Producto";
 
 export default function Productos() {
 
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     //Consultar la API
-    const cargarProductos = () => dispatch ( obtenerProductosAction() );
+    const cargarProductos = () => dispatch(obtenerProductosAction());
     cargarProductos();
   }, [])
+
+  //Obtener el State
+  const productos = useSelector(state => state.productos.productos);
+  const error = useSelector(state => state.productos.error)
+  const cargando = useSelector(state => state.productos.loading)
 
   return (
     <>
       <h2 className="text-center my-5">Listado de Productos</h2>
 
-      <table className="table table-striped">
+      { error && <p className="font-weight-bold alert alert-danger text-center mt-4 w-80">Hubo un error</p>}
+      { cargando && <p className="text-center">Cargando...</p>}
+
+      <table className="table table-striped w-80 mb-2">
         <thead className="bg-primary table-dark">
-          <tr>
+          <tr className="text-center">
             <th scope="col">Nombe</th>
             <th scope="col">Precio</th>
             <th scope="col">Acciones</th>
@@ -28,7 +37,16 @@ export default function Productos() {
         </thead>
 
         <tbody>
-
+          {productos.length === 0
+            ? "No hay productos"
+            : (
+              productos.map( producto => (
+                <Producto 
+                  key={producto.id}
+                  producto={producto}
+                />
+              ))
+            )}
         </tbody>
       </table>
     </>
